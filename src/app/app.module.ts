5fcae7515468e47b6703be320cdfdf9217f10bfd
Alex-Mac <alex.hongquan.xiao@gmail.com>
2017-09-07 15:00:12 -0700

    add more component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { DemoComponent } from './demo/demo.component';
+import { InformationComponent } from './information/information.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const router: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'demo', component: DemoComponent},
+  {path: 'information', component: InformationComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    DemoComponent,
+    InformationComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router),
   ],
   providers: [],
   bootstrap: [AppComponent]
