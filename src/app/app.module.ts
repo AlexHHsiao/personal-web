859eddcf5b70452dded6113496af810190969e98
Alex-Home <alex.hongquan.xiao@gmail.com>
2018-01-04 22:40:55 -0800

    reset the whole project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
-import { DemoComponent } from './demo/demo.component';
-import { InformationComponent } from './information/information.component';
 import {RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const router: Routes = [
   {path: '', component: MainComponent},
-  {path: 'demo', component: DemoComponent},
-  {path: 'information', component: InformationComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     MainComponent,
-    DemoComponent,
-    InformationComponent,
     PageNotFoundComponent
   ],
   imports: [
