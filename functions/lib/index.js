e6eaefc1e0af413b833c29f5ffb9f4f9fcd87f3d
Alex-Mac <alex.hongquan.xiao@gmail.com>
2018-02-24 00:41:41 -0800

    change firebase functions from js to ts

---
+++ functions/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+//# sourceMappingURL=index.js.map
