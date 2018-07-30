4a09e4a1827bd28f248b4dba3481c13e9918dec8
Alex Xiao <alex.xiao@elliemae.com>
2018-07-30 09:22:12 -0700

    finish with create room method starting with join room method

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -29,38 +29,58 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 // express functions
 const createRoom = (req, res) => __awaiter(this, void 0, void 0, function* () {
     const body = req.body;
+    // check necessary attribute
     if (typeof body.werewolf !== 'number'
         || typeof body.seer !== 'number'
         || typeof body.villager !== 'number'
         || typeof body.size !== 'number'
+        || typeof body.fool !== 'number'
+        || typeof body.hunter !== 'number'
+        || typeof body.witch !== 'number'
+        || typeof body.guard !== 'number'
         || typeof body.witchSaveSelf !== 'boolean') {
         return res.status(400).json({
             error: 'Please provide correct body attribute',
             code: 400
         });
     }
-    if (body.werewolf < 1
-        || body.villager < 1
-        || body.seer < 1
+    // necessary attribute value must be correct
+    if (body.werewolf < 2
+        || body.villager < 2
+        || body.seer < 2
         || body.size < game_1.MinRoomSize) {
         return res.status(400).json({
-            error: 'Werewolf, villager, or seer cannot be less than 1, ' +
+            error: 'Werewolf, villager, or seer cannot be less than 2, ' +
                 'and group size must be more than ' + game_1.MinRoomSize,
             code: 400
         });
     }
-    admin.firestore().collection('room').get().then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
-        });
-    });
+    const roomRef = admin.firestore().collection('room');
     const roomID = Math.random().toString().substr(2, 6);
-    return res.status(200).json({
+    const roomObj = {
+        gameEnd: false,
+        full: false,
+        players: [],
         roomID: roomID,
+        roomConfig: body
+    };
+    roomRef.doc(roomID).set(roomObj);
+    return res.status(200).json({
+        roomObj: roomObj,
         code: 200
     });
 });
 const joinRoom = (req, res) => __awaiter(this, void 0, void 0, function* () {
+    const body = req.body;
+    if (typeof body.roomID !== 'number') {
+        return res.status(400).json({
+            error: 'Please provide correct room id',
+            code: 400
+        });
+    }
+    return res.status(200).json({
+        code: 200
+    });
 });
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,10 +25,15 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 const createRoom = async (req, res) => {
   const body: RoomConfig = req.body;
 
+  // check necessary attribute
   if (typeof body.werewolf !== 'number'
     || typeof body.seer !== 'number'
     || typeof body.villager !== 'number'
     || typeof body.size !== 'number'
+    || typeof body.fool !== 'number'
+    || typeof body.hunter !== 'number'
+    || typeof body.witch !== 'number'
+    || typeof body.guard !== 'number'
     || typeof body.witchSaveSelf !== 'boolean') {
     return res.status(400).json({
       error: 'Please provide correct body attribute',
@@ -36,36 +41,50 @@ const createRoom = async (req, res) => {
     });
   }
 
-  if (body.werewolf < 1
-    || body.villager < 1
-    || body.seer < 1
+  // necessary attribute value must be correct
+  if (body.werewolf < 2
+    || body.villager < 2
+    || body.seer < 2
     || body.size < MinRoomSize) {
     return res.status(400).json({
-      error: 'Werewolf, villager, or seer cannot be less than 1, ' +
-      'and group size must be more than ' + MinRoomSize,
+      error: 'Werewolf, villager, or seer cannot be less than 2, ' +
+        'and group size must be more than ' + MinRoomSize,
       code: 400
     });
   }
 
-  admin.firestore().collection('room').get().then((snapshot) => {
-    snapshot.forEach((doc) => {
-
-
-      console.log(doc.id, '=>', doc.data());
-
-    });
-  });
-
+  const roomRef = admin.firestore().collection('room');
   const roomID = Math.random().toString().substr(2, 6);
 
-  return res.status(200).json({
+  const roomObj: Room = {
+    gameEnd: false,
+    full: false,
+    players: [],
     roomID: roomID,
+    roomConfig: body
+  };
+
+  roomRef.doc(roomID).set(roomObj);
+
+  return res.status(200).json({
+    roomObj: roomObj,
     code: 200
   });
 };
 
 const joinRoom = async (req, res) => {
+  const body = req.body;
 
+  if (typeof body.roomID !== 'number') {
+    return res.status(400).json({
+      error: 'Please provide correct room id',
+      code: 400
+    });
+  }
+
+  return res.status(200).json({
+    code: 200
+  });
 };
 
 // Automatically allow cross-origin requests
