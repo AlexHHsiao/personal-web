5d278b451039cd3ded5ee822e01150a8b8129423
Alex Xiao <alex.hongquan.xiao@gmail.com>
2018-10-14 14:27:55 -0700

    able to chat with serve

--- src/app/who-i-am/chat/chat.component.ts
+++ src/app/who-i-am/chat/chat.component.ts
@@ -19,26 +19,10 @@ export class ChatComponent implements OnInit {
     this.sendAllowed = true;
     this.client = new ApiAiClient({accessToken: 'f4d5b532c03c4c8183d98c605d202d9b'});
 
-    this.client.textRequest('hello').then((response) => {
-      console.log('answer', response);
-    }).catch((error) => {
-      console.log(error);
-    });
-
     const chatMessage = new ChatMessage(
       'This is Alex bot. I can answer you ang question you want about Alex. How can I help you?',
       false, new Date());
     this.chatService.setMessageCollection(chatMessage);
-
-    const chatMessage1 = new ChatMessage(
-      'This is Alex bot.',
-      false, new Date());
-    this.chatService.setMessageCollection(chatMessage1);
-
-    const chatMessage2 = new ChatMessage(
-      'This',
-      false, new Date());
-    this.chatService.setMessageCollection(chatMessage2);
   }
 
   ngOnInit() {
@@ -46,7 +30,6 @@ export class ChatComponent implements OnInit {
 
   sendMsg() {
     if (this.msg.trim().length !== 0) {
-
       if (this.sendAllowed) {
         this.sendAllowed = false;
         const chatMessage = new ChatMessage(this.msg, true, new Date());
@@ -56,6 +39,7 @@ export class ChatComponent implements OnInit {
         this.msg = '';
       } else {
         // cannot send tell user pls
+        // waiting for serve response
       }
     }
   }
@@ -67,10 +51,23 @@ export class ChatComponent implements OnInit {
   responseToUser(userMsg: string) {
     if (!this.sendAllowed) {
 
+      this.client.textRequest(userMsg).then((response) => {
+        const chatMessage = new ChatMessage(
+          response.result.fulfillment.speech,
+          false, new Date());
+        this.chatService.setMessageCollection(chatMessage);
 
-      this.sendAllowed = true;
+        this.sendAllowed = true;
+      }).catch((error) => {
+        const chatMessage = new ChatMessage(
+          'Sorry. I cannot answer your question now due to serve error!',
+          false, new Date());
+        this.chatService.setMessageCollection(chatMessage);
+
+        this.sendAllowed = true;
+      });
     } else {
-      alert('bug here??????');
+      alert('Something is wrong here!!!');
     }
   }
 }
